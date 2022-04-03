df9eca6d376369047a45d7c3f68891e432ee2abb
Eric Bangug <ericbangug@gmail.com>
2022-04-03 12:28:24 +0800

    Search functionality

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,15 +1,17 @@
 <x-dropdown>
     <x-slot name="trigger">
         <button class="py-2 pl-3 pr-9 text-sm font-semibold inline-flex w-full lg:w-32">
-            Categories
+            {{ $currentCategory ? ucwords($currentCategory->name) : 'Categories' }}
             <x-icons name="dropdown" />
         </button>
     </x-slot>
 
     <x-dropdown-item href="/">All</x-dropdown-item>
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?category={{ $category->slug }}" :active="request('category') === $category->slug">
-            {{ $category->name }}
+        <x-dropdown-item
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except(['category'])) }}"
+            :active="request('category') === $category->slug">
+            {{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
 </x-dropdown>
